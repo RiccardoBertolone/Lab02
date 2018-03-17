e08f9da45e9579c8582d4750f7e9801925676b37
rbert <rbert@DESKTOP-TN8H5FI.station>
2018-03-17 18:42:15 +0100

    versione 1.0

--- Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -17,6 +17,9 @@ import javafx.scene.control.TextField;
 
 public class AlienController {
 	
+	Dizionario dizionario = new Dizionario(); // creo il dizionario
+	
+	
     @FXML
     private ResourceBundle resources;
     @FXML
@@ -35,6 +38,7 @@ public class AlienController {
     void initialize() {
     	assert txtWord != null : "fx:id=\"txtWord\" was not injected: check your FXML file 'Alien.fxml'.";
     	assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Alien.fxml'.";
+    	txtResult.appendText("Welcome to Alien Dictionary v2016.\n");
     	assert btnTranslate != null : "fx:id=\"bntTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
     	assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
     	
@@ -43,13 +47,57 @@ public class AlienController {
     
     @FXML
     void doTranslate(ActionEvent event) {
-    	    	
+    	String word = txtWord.getText(); // prendo il testo dalla textField
+    	word=word.trim();
+    	
+    	
+    	
+    	if (containsWhiteSpace(word)) { // caso AGGIUNGI
+    		String[] words = word.split(" ");
+    		if (isAlpha(words[0]) && isAlpha(words[1])) { // se Lettere
+    			try {
+    				dizionario.aggiungiVoce(new Voce(words[0], words[1]));
+    				txtResult.appendText("Aggiunta al dizionario una traduzione di "+words[0]+"\n");
+    			} catch (TraduzioneGiaPresenteException e) {
+    				txtResult.appendText("Traduzione già presente nel dizionario\n");
+    			}
+    		}
+    		else 
+    			txtResult.appendText("Non inserire carateri speciali\n");
+    		
+    	 }
+    	
+    	else { // caso TRADUCI
+    		try {
+				txtResult.appendText("Traduzione di "+word+": "+dizionario.traduci(word)+"\n");
+			} catch (ParolaInesistenteException e) {
+				txtResult.appendText("Parola non presente nel dizionario\n");
+			}
+    	}
+    		
     }
-    
+
     
     @FXML
     void doReset(ActionEvent event) {
+    	
+    	txtResult.setText("Welcome to Alien Dictionary v2016.\n");
 
+    }
+    
+    public boolean isAlpha(String name) {
+    	return name.matches("[a-zA-Z]+");
+    }
+    
+    public boolean containsWhiteSpace (String testCode) {
+    	if (testCode !=null) {
+    		for (int i = 0; i<testCode.length(); i++) {
+    			if (Character.isWhitespace(testCode.charAt(i))) {
+    				return true;
+    			}
+    		}
+    	}
+    	return false;
     }
     
 }
--- Lab2_Alien/src/it/polito/tdp/alien/Test.java
+++ Lab2_Alien/src/it/polito/tdp/alien/Test.java
@@ -1,5 +1,12 @@
 package it.polito.tdp.alien;
 
 public class Test {
-
+	
+	public static void main(String[] args) {
+		
+		Dizionario d = new Dizionario();
+		
+		
+	}
+	
 }
